e5014af1e9a052392b355e186205d3be4ca118a5
alezcanossj <aledanielxiaomi@gmail.com>
2023-09-28 14:49:17 -0400

    Cambios de verificacion con nc

--- verificar_y_ejecutar.sh
+++ verificar_y_ejecutar.sh
@@ -4,7 +4,7 @@ PUERTO=21328;
 LOG_FILE=/home/locchase/repositories/ChaseTeltonika/log_bash.log
 FECHA_HORA=$(date +"%Y-%m-%d %H:%M:%S")
 # Verificar si el puerto está en uso
-if lsof -i :$PUERTO; then
+if nc -z -w1 127.0.0.1 $PUERTO; then
      echo "$FECHA_HORA: El puerto $PUERTO está en uso." >> $LOG_FILE
 else
     echo "$FECHA_HORA: El puerto $PUERTO no está en uso. Ejecutando archivo PHP." >> $LOG_FILE
