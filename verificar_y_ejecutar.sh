77a0ba7364be986da52ce39b4c798bd05edad520
alezcanossj <aledanielxiaomi@gmail.com>
2023-09-28 14:24:46 -0400

    Cambios para guardar logs

--- verificar_y_ejecutar.sh
+++ verificar_y_ejecutar.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 PUERTO=21328;
-
+LOG_FILE=/home/locchase/repositories/ChaseTeltonika/log_bash.log
+FECHA_HORA=$(date +"%Y-%m-%d %H:%M:%S")
 # Verificar si el puerto está en uso
 if lsof -i :$PUERTO; then
-    echo "El puerto $PUERTO está en uso."
+     echo "$FECHA_HORA: El puerto $PUERTO está en uso." >> $LOG_FILE
 else
-    echo "El puerto $PUERTO no está en uso."
+    echo "$FECHA_HORA: El puerto $PUERTO no está en uso. Ejecutando archivo PHP." >> $LOG_FILE
     php /home/locchase/repositories/ChaseTeltonika/run.php
 fi
